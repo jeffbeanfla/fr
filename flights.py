af6ffec86b551a7e34f2066b3d204c1fea289e1a
vboxuser <vboxuser@Linux.myguest.virtualbox.org>
2025-10-14 22:03:15 -0400

    collin is smart

--- flights.py
+++ flights.py
@@ -2,7 +2,9 @@ from FlightRadar24 import FlightRadar24API,Countries
 fr_api = FlightRadar24API()
 
 flights =  fr_api.get_flights()
-
+x = 0
 for flight in flights:
-    print({flight.id})
-
+    x += 1
+    print({flight.callsign})
+    if x > 10 :
+        break
