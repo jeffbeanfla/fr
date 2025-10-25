876daae4d56fb8070548d7d8096e4ed0943f4336
wizard <jeffbeanfla@gmail.com>
2025-10-24 22:57:38 -0400

    MiamiBox

--- flights.py
+++ flights.py
@@ -1,6 +1,9 @@
 from FlightRadar24 import FlightRadar24API, Countries
 import time
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+import operator
+from datetime import datetime
+import requests
 
 # rbg options
 options = RGBMatrixOptions()
@@ -36,7 +39,7 @@ fr_api = FlightRadar24API()
 flights =  fr_api.get_flights()
 #airports = fr_api.get_airports([Countries.UNITED_STATES])
 
-
+'''
 x = 0
 for f in flights:
     #d=fr_api.get_flight_details (f)
@@ -49,7 +52,52 @@ for f in flights:
         if f.origin_airport_iata and f.altitude > 0:
             x += 1
             print(f.number,f.origin_airport_iata,f.destination_airport_iata,f.altitude)
+ '''
+airports = fr_api.get_airports([Countries.UNITED_STATES])
+
+for a in airports:
+      if a.iata == 'MIA':
+             #print(a.name,a.iata,a.icao,a.latitude,a.longitude)
+             ar = a
+             
+bounds=fr_api.get_bounds_by_point(25.7,-80.50,100000)
+#bounds=fr_api.get_bounds_by_point(26.07365,-80.15153,100000)  #FLL
+#time_left==0
+#ft==0
+
+min_lat=25.70#26.00
+max_lat=26.00#26.20
+min_lon=-80.25#-79.00
+max_lon=-80.60#-80.60
+
+flights=fr_api.get_flights(bounds = bounds)
+x = 0
+for flight in flights:
+
+#   if flight.number == "DL188":
+    f=flight
+    d=fr_api.get_flight_details (f)
+    distance=f.get_distance_from (ar)
+    f.set_flight_details(d)
+     
+    time_left=time.time()-f.time
+
+    try:
+        minutes=(distance/f.ground_speed)*60
+        tt=(distance/f.ground_speed)
+    except:
+        result=0
         
+    ct=datetime.fromtimestamp(minutes)
+    ft=ct.strftime('%M:%S')
+
+    if f.destination_airport_iata == 'MIA' and operator.gt(distance,5) and min_lat<= f.latitude <=max_lat and min_lon>=f.longitude >=max_lon:
+            x += 1
+            print("FlightNo",f.number, "From",f.origin_airport_iata,"To",f.destination_airport_iata,"Dist",distance, "Alt",f.altitude,"Speed",f.ground_speed,"Type",f.aircraft_model,"Time Left",ft,f.latitude, f.longitude)
+            #if min_lat<= f.latitude <=max_lat and min_lon>=f.longitude >=max_lon:
+                #print("Y")
+
+
             #create canvas with text
             canvas = matrix.CreateFrameCanvas()
             graphics.DrawText(canvas, font, 1, 10, textColor1, f.origin_airport_iata)
