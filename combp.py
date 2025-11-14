99c2ec4d51a58e2ca05e00c50961d46e45aad425
wizard <jeffbeanfla@gmail.com>
2025-11-13 22:19:43 -0500

    not much

--- combp.py
+++ combp.py
@@ -144,14 +144,57 @@ while True:
                 graphics.DrawText(canvas, font, 1, 20, textColor2, "Speed " + str(f.ground_speed))
                 graphics.DrawText(canvas, font, 1, 30, textColor3, "Alt " + str(f.altitude))
 
+
+                
+    
                 matrix.Clear()
                 matrix.SwapOnVSync(canvas)
                 time.sleep(5)
                 matrix.Clear()
+                
+                async def main():
+                    async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
+                        weather = await client.get('Fort Lauderdale')
+        #now=time.localtime()
+                        now=datetime.now()
+                        ftoday= now.strftime("%-I:%M%p")
+        
+                        print ("Today:",date.today(),"At:",ftoday ,"Temp:", weather.temperature,  "Outlook: ", weather.kind)
+        
+                        ti=ftoday
+         
+                        str=date.today()
+                        d=str.isoformat()
+                        d=str.strftime("%m/%d/%y")
+                        k=f"{weather.kind}"
+                        dd=str.strftime('%a')
+                        deg_sym = "\u00b0"
+        
+                        te=f"{weather.temperature}{deg_sym}"
+        
+        
+        
+                        canvas = matrix.CreateFrameCanvas()
+                        graphics.DrawText(canvas, font5, 1, 10, textColor1, d + ' ' + dd)     
+                        graphics.DrawText(canvas, font5, 1, 20, textColor2, ti + ' ' + te)
+                        graphics.DrawText(canvas, font4, 1, 30, textColor3, k)
+
+                        mtrix.Clear()
+                        matrix.SwapOnVSync(canvas)
+                        time.sleep(5)
+                        #matrix.Clear()
+    
+                        asyncio.run(main())
+                
                 if x > 20 :
                     break
     end_time=time.time()
     print(end_time-start_time)
+    
+    #canvas = matrix.CreateFrameCanvas()
+    #graphics.DrawText(canvas, font5, 1, 10, textColor1, d + ' ' + dd)     
+    #graphics.DrawText(canvas, font5, 1, 20, textColor2, ti + ' ' + te)
+    #graphics.DrawText(canvas, font4, 1, 30, textColor3, k)
 time.sleep(60)
 
 
